bbe5a5eee5ee9615f337d7f1ebdbcae15c9c4b80
Andrew Chok <chok.n.drew@gmail.com>
2019-09-28 21:23:09 -0400

    added laser

--- IGSJ/Assets/DTR/VRControllerManager.cs
+++ IGSJ/Assets/DTR/VRControllerManager.cs
@@ -8,19 +8,33 @@ public class VRControllerManager : MonoBehaviour
     public SteamVR_Input_Sources handType;
     public SteamVR_Behaviour_Pose controllerPose;
     public SteamVR_Action_Boolean grabAction;
+    public SteamVR_Action_Boolean teleportAction;
 
+    #region Grabbing Objects
     private GameObject collidingObject; // 1
     private GameObject objectInHand; // 2
+    #endregion
 
+    #region Laser Pointer
+    public GameObject laserPrefab; // 1
+    private GameObject laser; // 2
+    private Transform laserTransform; // 3
+    private Vector3 hitPoint; // 4
+
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 1
+        laser = Instantiate(laserPrefab);
+        // 2
+        laserTransform = laser.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        #region Grabbing
         // 1
         if (grabAction.GetLastStateDown(handType))
         {
@@ -38,7 +52,50 @@ public class VRControllerManager : MonoBehaviour
                 ReleaseObject();
             }
         }
+
+        #endregion
+
+        #region Movement
+        if (teleportAction.GetState(handType))
+        {
+            RaycastHit hit;
+
+            // 2
+            if (Physics.Raycast(controllerPose.transform.position, transform.forward, out hit, 100))
+            {
+                hitPoint = hit.point;
+                ShowLaser(hit);
+            }
+            else // 3
+            {
+                laser.SetActive(false);
+            }
+        }
+        else // 3
+        {
+            laser.SetActive(false);
+        }
+        #endregion
     }
+
+    #region Movement
+    private void ShowLaser(RaycastHit hit)
+    {
+        // 1
+        laser.SetActive(true);
+        // 2
+        laserTransform.position = Vector3.Lerp(controllerPose.transform.position, hitPoint, .5f);
+        // 3
+        laserTransform.LookAt(hitPoint);
+        // 4
+        laserTransform.localScale = new Vector3(laserTransform.localScale.x,
+                                                laserTransform.localScale.y,
+                                                hit.distance);
+    }
+    #endregion
+
+
+    #region grabbing objects
 
     private void SetCollidingObject(Collider col)
     {
@@ -108,4 +165,6 @@ public class VRControllerManager : MonoBehaviour
         // 4
         objectInHand = null;
     }
+
+    #endregion
 }
