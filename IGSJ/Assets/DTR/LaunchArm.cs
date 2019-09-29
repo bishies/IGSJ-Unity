eee2ce2ffa72ead4d5df547bae52dcd5e81e347d
bishies <33171456+bishies@users.noreply.github.com>
2019-09-28 22:29:53 -0400

    player and launch arm

---
+++ IGSJ/Assets/DTR/LaunchArm.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LaunchArm : MonoBehaviour
+{
+    public GameObject arm;
+    public float launchSpeed = 40f;
+
+
+    public void launchArm()
+    {
+        arm.transform.position = arm.transform.position + arm.transform.forward * 2;
+        Rigidbody rb = arm.GetComponent<Rigidbody>();
+        rb.velocity = arm.transform.forward * launchSpeed;
+    }
+}
