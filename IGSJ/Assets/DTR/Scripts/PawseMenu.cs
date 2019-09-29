a42c929afda2fb9d90f4c1f9918b93af89a0436f
Tho Tran <cinnar12@gmail.com>
2019-09-29 13:58:25 -0400

    UI sounds and BGM in, prefab for pause menu in

---
+++ IGSJ/Assets/DTR/Scripts/PawseMenu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PawseMenu : MonoBehaviour
+{
+    public static bool isGamePawsed = false;
+
+    public GameObject pawseMenuUI;
+
+    public string menuScene;
+
+    public AudioSource resumeSound;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isGamePawsed)
+            {
+                Resume();
+                resumeSound.Play();
+
+            } else
+            {
+                Pawse();
+            }
+        }
+    }
+
+    public void Resume ()
+    {
+        pawseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePawsed = false;
+    }
+
+    void Pawse ()
+    {
+        pawseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePawsed = true;
+    }
+
+    public void LoadMenu ()
+    {
+        StartCoroutine(DelayedLoadMenu());
+    }
+
+    public void QuitGame()
+    {
+        StartCoroutine(DelayedQuitGame());
+    }
+
+    IEnumerator DelayedLoadMenu()
+    {
+        Time.timeScale = 1f;
+        yield return new WaitForSeconds(0.15f);
+        SceneManager.LoadScene(menuScene);
+    }
+
+    IEnumerator DelayedQuitGame()
+    {
+        Time.timeScale = 1f;
+        yield return new WaitForSeconds(0.15f);
+        Application.Quit();
+    }
+}
