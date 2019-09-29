6f90e297c2bd8f55cd624ea20d91cab065b5533c
Andrew Chok <chok.n.drew@gmail.com>
2019-09-28 22:25:06 -0400

    Grappling works

--- IGSJ/Assets/DTR/VRControllerManager.cs
+++ IGSJ/Assets/DTR/VRControllerManager.cs
@@ -21,7 +21,34 @@ public class VRControllerManager : MonoBehaviour
     private Transform laserTransform; // 3
     private Vector3 hitPoint; // 4
 
+    // 1
+    public Transform cameraRigTransform;
+    // 2
+    public GameObject teleportReticlePrefab;
+    // 3
+    private GameObject reticle;
+    // 4
+    private Transform teleportReticleTransform;
+    // 5
+    public Transform headTransform;
+    // 6
+    public Vector3 teleportReticleOffset;
+    // 7
+    public LayerMask teleportMask;
+    // 8
+    private bool shouldTeleport;
     #endregion
+
+    #region Grappling
+    Vector3 start_pos = Vector3.zero;
+    Vector3 final_pos = Vector3.zero;
+
+    float fractionOfReset = 0;
+    float speedOfGrapple = 0.02f;
+
+    private bool isGrapplingOver = false;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +56,10 @@ public class VRControllerManager : MonoBehaviour
         laser = Instantiate(laserPrefab);
         // 2
         laserTransform = laser.transform;
+        // 1
+        reticle = Instantiate(teleportReticlePrefab);
+        // 2
+        teleportReticleTransform = reticle.transform;
     }
 
     // Update is called once per frame
@@ -61,19 +92,48 @@ public class VRControllerManager : MonoBehaviour
             RaycastHit hit;
 
             // 2
-            if (Physics.Raycast(controllerPose.transform.position, transform.forward, out hit, 100))
+            if (Physics.Raycast(controllerPose.transform.position, transform.forward, out hit, 100, teleportMask))
             {
                 hitPoint = hit.point;
                 ShowLaser(hit);
+                // 1
+                reticle.SetActive(true);
+                // 2
+                teleportReticleTransform.position = hitPoint + teleportReticleOffset;
+                // 3
+                shouldTeleport = true;
             }
             else // 3
             {
                 laser.SetActive(false);
+                reticle.SetActive(false);
             }
         }
         else // 3
         {
             laser.SetActive(false);
+            reticle.SetActive(false);
+        }
+
+        if (teleportAction.GetStateUp(handType) && shouldTeleport && !isGrapplingOver)
+        {
+            Teleport();
+        }
+
+        if(isGrapplingOver)
+        {
+            if(cameraRigTransform.position != final_pos)
+            {
+                fractionOfReset += speedOfGrapple;
+                if (fractionOfReset >= 1) fractionOfReset = 1;
+                cameraRigTransform.position = Vector3.Lerp(start_pos, final_pos, fractionOfReset);
+            }
+            else
+            {
+                isGrapplingOver = false;
+                cameraRigTransform.position = final_pos;
+                shouldTeleport = false;
+            }
         }
         #endregion
     }
@@ -92,8 +152,37 @@ public class VRControllerManager : MonoBehaviour
                                                 laserTransform.localScale.y,
                                                 hit.distance);
     }
+    private void Teleport()
+    {
+        reticle.SetActive(false);
+        // 1
+        // 3
+        Vector3 difference = cameraRigTransform.position - headTransform.position;
+        // 4
+        difference.y = 0;
+        // 5
+        start_pos = cameraRigTransform.position;
+        final_pos = hitPoint + difference;
+        //cameraRigTransform.position = hitPoint + difference;
+
+        fractionOfReset = 0;
+        speedOfGrapple = 0.02f;
+        float distance = Vector3.Distance(final_pos, start_pos);
+
+        if(distance < 10)
+        {
+            speedOfGrapple = 0.04f;
+        }
+
+        isGrapplingOver = true;
+    }
+
     #endregion
 
+    IEnumerator GrapplingHook()
+    {
+        yield return new WaitForSeconds(1);
+    }
 
     #region grabbing objects
 
